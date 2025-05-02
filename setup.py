9629cf94f5e62fe757e0383339d63e319fc7a4c0
Mirellys <mirellysd@spotify.com>
2025-05-02 17:19:53 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai-utils",
+    version="0.1.0",  # Initial version
+    description="A utility package for ai-utils",
+    author="Mirellys Arteta Davila",
+    author_email="mirellys710@gmail.com",
+    url="https://github.com/syllerim/ai-utils",
+    packages=find_packages(),  # Automatically find Python modules
+    install_requires=[
+        "gzip",
+        "os",
+        "requests",
+        "pandas"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
