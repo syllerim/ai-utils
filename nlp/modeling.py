42a91bab092cf2fbc2a7ff86958e3e1e58ed9339
syllerim <mirellysd@spotify.com>
2025-06-14 18:39:13 +0100

    Update modeling.py
    save also test_target dataset in different csv

--- nlp/modeling.py
+++ nlp/modeling.py
@@ -19,20 +19,24 @@ def save_dataframe_csv(df, path):
 
 # --------------- process_and_save_splits ---------------
 
-def process_and_save_splits(df, name, full_path, train_path, test_path, test_size=0.25, random_state=0):
+def process_and_save_splits(df, name, target, full_path, train_path, test_path, test_target_path, test_size=0.25, random_state=0):
     print(f"\n📁 Processing dataset: {name}")
 
     # save the full dataframe
     save_dataframe_csv(df, full_path)
 
     # split into train and test
-    train_df, test_df = train_test_split(df, test_size=test_size, shuffle=True, random_state=random_state)
+    train_df, test_df = train_test_split(df, test_size=test_size, shuffle=True, random_state=random_state, stratify=df[target])
     print(f"{name} train shape: {train_df.shape}, test shape: {test_df.shape}")
 
-    # save train and test splits
+    # save train set (with target)
     save_dataframe_csv(train_df, train_path)
-    save_dataframe_csv(test_df, test_path)
 
+    # save test set without target
+    save_dataframe_csv(test_df.drop(columns=[target]), test_path)
+
+    # save test target separately
+    save_dataframe_csv(test_df[[target]], test_target_path)
 
 # --------------- load_and_extract_tokens ---------------
 
